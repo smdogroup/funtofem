66b4d69abe2663b6cf4d6eacf07ef9ebf0af6db0
lhalim6 <lhalim6@gatech.edu>
2021-02-26 11:21:18 -0500

    updated licensing and comments

--- pyfuntofem/solver_interface.py
+++ pyfuntofem/solver_interface.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python
-"""
-This file is part of the package FUNtoFEM for coupled aeroelastic simulation
-and design optimization.
 
-Copyright (C) 2015 Georgia Tech Research Corporation.
-Additional copyright (C) 2015 Kevin Jacobson, Jan Kiviaho and Graeme Kennedy.
-All rights reserved.
+# This file is part of the package FUNtoFEM for coupled aeroelastic simulation
+# and design optimization.
 
-FUNtoFEM is licensed under the Apache License, Version 2.0 (the "License");
-you may not use this software except in compliance with the License.
-You may obtain a copy of the License at
+# Copyright (C) 2015 Georgia Tech Research Corporation.
+# Additional copyright (C) 2015 Kevin Jacobson, Jan Kiviaho and Graeme Kennedy.
+# All rights reserved.
 
-   http://www.apache.org/licenses/LICENSE-2.0
+# FUNtoFEM is licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this software except in compliance with the License.
+# You may obtain a copy of the License at
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-"""
+#   http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 class SolverInterface(object):
     """
