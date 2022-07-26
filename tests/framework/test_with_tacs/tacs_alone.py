85e83e94aee6781e99bab06ccca510dac393744d
Burke, Brian <bburke38@gatech.edu>
2022-07-26 16:30:15 -0400

    Organize tests for new formulation
    Organize framework tests into separate folders.
    Add test to run TACS with the internal test flow solver.

--- tests/framework/test_with_tacs/tacs_alone.py
+++ tests/framework/test_with_tacs/tacs_alone.py
@@ -1,11 +1,31 @@
-from mpi4py import MPI
+
+from __future__ import print_function
+
 from pyfuntofem.funtofem_model import FUNtoFEMmodel
 from pyfuntofem.variable import Variable
 from pyfuntofem.scenario import Scenario
 from pyfuntofem.body import Body
 from pyfuntofem.function import Function
-from pyfuntofem.test_solver import TestSolver
 from pyfuntofem.funtofem_nlbgs_driver import FUNtoFEMnlbgs
+from pyfuntofem.test_solver import TestSolver
+
+from tacs_model import wedgeTACS
+from mpi4py import MPI
+import os, sys
+import time
+
+# Split the communicator
+n_tacs_procs = 1
+comm = MPI.COMM_WORLD
+
+world_rank = comm.Get_rank()
+if world_rank < n_tacs_procs:
+    color = 55
+    key = world_rank
+else:
+    color = MPI.UNDEFINED
+    key = world_rank
+tacs_comm = comm.Split(color, key)
 
 # Build the model
 model = FUNtoFEMmodel("wedge")
@@ -18,22 +38,19 @@ plate.add_variable("structural", svar)
 model.add_body(plate)
 
 # Create a scenario to run
-steady = Scenario("steady", group=0, steps=100)
+steps = 20
+steady = Scenario("steady", group=0, steps=steps)
 
 # Add a function to the scenario
-temp = Function("temperature", analysis_type="structural")
-steady.add_function(temp)
-
 temp = Function("temperature", analysis_type="structural")
 steady.add_function(temp)
 
 model.add_scenario(steady)
 
 # Instantiate a test solver for the flow and structures
-comm = MPI.COMM_WORLD
 solvers = {}
 solvers["flow"] = TestSolver(comm, model, solver="flow")
-solvers["structural"] = TestSolver(comm, model, solver="structure")
+solvers["structural"] = wedgeTACS(comm, tacs_comm, model, n_tacs_procs)
 
 # L&D transfer options
 transfer_options = {
@@ -44,6 +61,8 @@ transfer_options = {
 }
 
 # instantiate the driver
-driver = FUNtoFEMnlbgs(solvers, comm, comm, 0, comm, 0, transfer_options, model=model)
+driver = FUNtoFEMnlbgs(solvers, comm, tacs_comm, 0, comm, 0, transfer_options, model=model)
 
-driver.solve_forward()
+fail = driver.solve_forward()
+if fail == 1:
+    print("\nSimulation failed.\n")
