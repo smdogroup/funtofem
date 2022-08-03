added code with new convention for adjoint-jacobian products (ajps) - fixed the test aero and structural solvers for both aeroelastic and aerothermoelastic analysis

pyfuntofem/body.py
@@ -561,20 +561,28 @@ class Body(Base):
             ns = 3 * self.struct_nnodes
             na = 3 * self.aero_nnodes
 
-            self.struct_loads_adj_prod = np.zeros((ns, nf), dtype=self.dtype)
-            self.aero_loads_adj_prod = np.zeros((na, nf), dtype=self.dtype)
-            self.struct_disps_adj_prod = np.zeros((ns, nf), dtype=self.dtype)
-            self.aero_disps_adj_prod = np.zeros((na, nf), dtype=self.dtype)
-            self.struct_loads_self_adj_prod = np.zeros((ns, nf), dtype=self.dtype)
+            self.struct_loads_ajp = np.zeros((ns, nf), dtype=self.dtype)
+            self.aero_loads_ajp = np.zeros((na, nf), dtype=self.dtype)
+            self.struct_disps_ajp = np.zeros((ns, nf), dtype=self.dtype)
+            self.aero_disps_ajp = np.zeros((na, nf), dtype=self.dtype)
+
+            # There are two contributions to the term struct_disps_ajp. We compute and
+            # store them separately and then add the contributions. These two contributions
+            # are from the displacement and load transfer, respectively and are computed in
+            # the transfer_disps_adjoint and transfer_loads_adjoint. Since they are computed
+            # at different times and we don't impose a calling order/requirement we have to
+            # store them separately.
+            self.struct_disps_ajp_disps = np.zeros((ns, nf), dtype=self.dtype)
+            self.struct_disps_ajp_loads = np.zeros((ns, nf), dtype=self.dtype)
 
         if self.thermal_transfer is not None:
             ns = self.struct_nnodes
             na = self.aero_nnodes
 
-            self.struct_flux_adj_prod = np.zeros((ns, nf), dtype=self.dtype)
-            self.aero_flux_adj_prod = np.zeros((na, nf), dtype=self.dtype)
-            self.struct_temps_adj_prod = np.zeros((ns, nf), dtype=self.dtype)
-            self.aero_temps_adj_prod = np.zeros((na, nf), dtype=self.dtype)
+            self.struct_flux_ajp = np.zeros((ns, nf), dtype=self.dtype)
+            self.aero_flux_ajp = np.zeros((na, nf), dtype=self.dtype)
+            self.struct_temps_ajp = np.zeros((ns, nf), dtype=self.dtype)
+            self.aero_temps_ajp = np.zeros((na, nf), dtype=self.dtype)
 
         return
 
@@ -820,12 +828,12 @@ class Body(Base):
 
         return
 
-    def get_aero_loads_adj_product(self, scenario):
+    def get_aero_loads_ajp(self, scenario):
         """
         Get the product of the load transfer adjoint variables with the derivative
         of the load transfer residuals with respect to the aerodynamic forces
 
-        aero_loads_adj_prod = dL/dfA^{T} * psi_L
+        aero_loads_ajp = dL/dfA^{T} * psi_L
 
         The value of this product is computed during the adjoint load
         transfer call.
@@ -837,31 +845,22 @@ class Body(Base):
         """
 
         if self.transfer is not None:
-            return self.aero_loads_adj_prod
+            return self.aero_loads_ajp
 
         return None
 
-    def get_aero_disps_adj_product(self, scenario):
+    def get_aero_disps_ajp(self, scenario):
         """
-        Get the product of the adjoint variables with the grid residuals with the derivative of the
-        displacement at the aerodynamic surface nodes
+        Get the product of the adjoint variables with the grid residuals with the
+        derivative of the displacement at the aerodynamic surface nodes
 
-        aero_disps_adj_prod = dG/duA^{T} * psi_G
+        aero_disps_ajp = dG/duA^{T} * psi_G
 
         Note that this term is typically just the shape sensitivity
 
         dG/duA^{T} * psi_G = dG/dxA0 * psi_G
-        """
 
-        if self.transfer is not None:
-            return self.aero_disps_adj_prod
-
-        return None
-
-    def get_struct_loads_adj_product(self, scenario):
-        """
-
-        struct_loads_adj_prod = dS/dfS^{T} * psi_S
+        This term must be set by the aerodynamic analysis.
 
         Parameters
         ----------
@@ -870,14 +869,15 @@ class Body(Base):
         """
 
         if self.transfer is not None:
-            return self.struct_loads_adj_prod
+            return self.aero_disps_ajp
 
         return None
 
-    def get_struct_disps_adj_product(self, scenario):
+    def get_struct_loads_ajp(self, scenario):
         """
+        Get the product of the structural adjoint variables
 
-        struct_disps_adj_prod = dD/duS^{T} * psi_D
+        struct_loads_ajp = dS/dfS^{T} * psi_S
 
         Parameters
         ----------
@@ -886,46 +886,64 @@ class Body(Base):
         """
 
         if self.transfer is not None:
-            return self.struct_disps_adj_prod
+            return self.struct_loads_ajp
 
         return None
 
-    def get_struct_loads_self_adj_product(self, scenario):
+    def get_struct_disps_ajp(self, scenario):
         """
+        Get the product of the displacement transfer adjoint variables with the derivative
+        of the displacement transfer residuals w.r.t. the structural displacements.
 
-        struct_loads_self_adj_prod = dL/duS^{T} * psi_L
+        struct_disps_ajp = dD/duS^{T} * psi_D + dL/duS^{T} * psi_L
+
+        Parameters
+        ----------
+        scenario: :class:`~scenario.Scenario`
+            The current scenario
         """
 
         if self.transfer is not None:
-            return self.struct_loads_self_adj_prod
+            return self.struct_disps_ajp
 
         return None
 
-    def get_aero_flux_adj_product(self, scenario):
+    def get_struct_loads_self_ajp(self, scenario):
+        """
 
-        if self.thermal_transfer is not None:
-            return self.aero_flux_adj_prod
+        struct_loads_disp_ajp = dL/duS^{T} * psi_L
+        """
 
-        return None
-
-    def get_struct_flux_adj_product(self, scenario):
-
-        if self.thermal_transfer is not None:
-            return self.struct_flux_adj_prod
+        if self.transfer is not None:
+            return self.struct_loads_self_ajp
 
         return None
 
-    def get_aero_temps_adj_product(self, scenario):
+    def get_aero_flux_ajp(self, scenario):
 
         if self.thermal_transfer is not None:
-            return self.aero_temps_adj_prod
+            return self.aero_flux_ajp
 
         return None
 
-    def get_struct_temps_adj_product(self, scenario):
+    def get_struct_flux_ajp(self, scenario):
 
         if self.thermal_transfer is not None:
-            return self.struct_temps_adj_prod
+            return self.struct_flux_ajp
+
+        return None
+
+    def get_aero_temps_ajp(self, scenario):
+
+        if self.thermal_transfer is not None:
+            return self.aero_temps_ajp
+
+        return None
+
+    def get_struct_temps_ajp(self, scenario):
+
+        if self.thermal_transfer is not None:
+            return self.struct_temps_ajp
 
         return None
 
@@ -934,19 +952,19 @@ class Body(Base):
         Perform the adjoint computation for the load transfer operation. This code
         computes and stores the required Jacobian-adjoint products.
 
-        Find the load transfer adjoint variables, psi_L, by solving the equation:
+        Find the load transfer adjoint variables, psi_L, by solving the equation
 
         dL/dfS^{T} * psi_L = - dS/dfS^{T} * psi_S
 
         The right-hand-side term is stored as
 
-        struct_loads_adj_prod = dS/dfS^{T} * psi_S
+        struct_loads_ajp = dS/dfS^{T} * psi_S
 
         and the Jacobian dL/dfS = I. After computing psi_L, the code computes the
         Jacobian-adjoint products
 
-        aero_loads_adj_prod = dL/dfA^{T} * psi_L
-        struct_loads_self_adj_prod = dL/duS^{T} * psi_L
+        aero_loads_ajp = dL/dfA^{T} * psi_L
+        struct_disps_ajp_loads = dL/duS^{T} * psi_L
         """
         nfunctions = scenario.count_adjoint_functions()
 
@@ -958,15 +976,20 @@ class Body(Base):
             for k in range(nfunctions):
                 # Solve for psi_L - Note that dL/dfS is the identity matrix. Ensure that
                 # the vector is in contiguous memory.
-                psi_L = -self.struct_loads_adj_prod[:, k].copy()
+                psi_L = -self.struct_loads_ajp[:, k].copy()
 
-                # Compute aero_loads_adj_prod = dL/dfa^{T} * psi_L
+                # Compute aero_loads_ajp = dL/dfa^{T} * psi_L
                 self.transfer.applydLdfATrans(psi_L, temp_fa)
-                self.aero_loads_adj_prod[:, k] = temp_fa
+                self.aero_loads_ajp[:, k] = temp_fa
 
-                # Compute struct_loads_self_adj_prod = dL/dus^{T} * psi_L
+                # Compute struct_disps_ajp_loads = dL/dus^{T} * psi_L
                 self.transfer.applydLduSTrans(psi_L, temp_us)
-                self.struct_loads_self_adj_prod[:, k] = temp_us
+                self.struct_disps_ajp_loads[:, k] = temp_us
+
+            # Compute the update to the struct_disps_ajp
+            self.struct_disps_ajp[:] = (
+                self.struct_disps_ajp_disps[:] + self.struct_disps_ajp_loads[:]
+            )
 
         return
 
@@ -981,12 +1004,12 @@ class Body(Base):
 
         Here the right-hand-side term is stored as
 
-        aero_disps_adj_prod = dG/duA^{T} * psi_G
+        aero_disps_ajp = dG/duA^{T} * psi_G
 
         The Jacobian dD/duA = I. After computing psi_D, the code computes the Jacobian-adjoint
         products
 
-        struct_disps_adj_prod = dD/duS^{T} * psi_D
+        struct_disps_ajp_disps = dD/duS^{T} * psi_D
         """
         nfunctions = scenario.count_adjoint_functions()
 
@@ -996,48 +1019,85 @@ class Body(Base):
             for k in range(nfunctions):
                 # Solve for psi_D - Note that dD/dua is the identity matrix
                 # Copy the values into contiguous memory.
-                psi_D = -self.aero_disps_adj_prod[:, k].copy()
+                psi_D = -self.aero_disps_ajp[:, k].copy()
 
                 # Set the dD/duS^{T} * psi_D product
                 self.transfer.applydDduSTrans(psi_D, temp_us)
-                self.struct_disps_adj_prod[:, k] -= temp_us
+                self.struct_disps_ajp_disps[:, k] = temp_us
+
+            # Compute the update to the struct_disps_ajp
+            self.struct_disps_ajp[:] = (
+                self.struct_disps_ajp_disps[:] + self.struct_disps_ajp_loads[:]
+            )
 
         return
 
     def transfer_heat_flux_adjoint(self, scenario, step):
+        """
+        Perform the adjoint computation for the heat flux transfer operations.
+
+        This function first solves for the heat flux transfer adjoint, psi_Q by solving
+        the equation
+
+        dQ/dhS^{T} * psi_Q = - dS/dhS^{T} * psi_S
+
+        Here the right-hand-side is stored as
+
+        struct_flux_ajp = dS/dhS^{T} * psi_S
+
+        Since dQ/dhS = I, the adjoint variables are psi_Q = - struct_flux_ajp.
+
+        The code then computes
+
+        aero_flux_ajp = dQ/dhA^{T} * psi_Q
+        """
+
         nfunctions = scenario.count_adjoint_functions()
 
-        #        if self.thermal_transfer is not None:
+        if self.thermal_transfer is not None:
+            temp = np.zeros(self.aero_nnodes, dtype=self.dtype)
 
-        # for func in range(nfunctions):
-        #     if body.thermal_transfer is not None:
-        #         # Transform heat flux transfer adjoint variables using transpose Jacobian from
-        #         # funtofem: dQdftA^T * psi_Q = dTdts * psi_Q
-        #         psi_Q_r = np.zeros(body.aero_nnodes, dtype=TransferScheme.dtype)
-        #         body.thermal_transfer.applydQdqATrans(
-        #             body.psi_T_S[:, func].copy(order="C"), psi_Q_r
-        #         )
+            for k in range(nfunctions):
+                psi_Q = -self.struct_flux_ajp[:, k].copy()
 
-        #         # Only set heat flux magnitude component of thermal adjoint in FUN3D
-        #         # Can either use surface normal magnitude OR x,y,z components, not both
-        #         body.dQdfta[:, func] = psi_Q_r
+                # In the MELD interface code qA = hA so applydQdqATrans could/should?
+                # be called applydQdhATrans
+                self.thermal_transfer.applydQdqATrans(psi_Q, temp)
+                self.aero_flux_ajp[:, k] = temp
 
         return
 
     def transfer_temps_adjoint(self, scenario, step):
+        """
+        Perform the adjoint computations for the thermal transfer operation.
 
-        # for func in range(nfunctions):
-        #     if body.thermal_transfer is not None:
-        #         # calculate dTdt_s^T * psi_T
-        #         psi_T_product = np.zeros(
-        #             body.struct_nnodes * body.therm_xfer_ndof,
-        #             dtype=TransferScheme.dtype,
-        #         )
-        #         body.psi_T = body.dAdta
-        #         body.thermal_transfer.applydTdtSTrans(
-        #             body.psi_T[:, func].copy(order="C"), psi_T_product
-        #         )
-        #         body.struct_rhs_T[:, func] = psi_T_product
+        This function first solves for the temperature transfer adjoint psi_T, by solving
+        the equation
+
+        dT/dtA^{T} * psi_T = - dA/dtA^{T} * psi_A
+
+        Here the right-hand-side is stored as
+
+        aero_temps_ajp = dA/dtA^{T} * psi_A
+
+        Since dT/dtA = I, then psi_T = - aero_temps_ajp.
+
+        The code then computes
+
+        struct_temps_ajp = dT/dtS^{T} * psi_T
+        """
+
+        nfunctions = scenario.count_adjoint_functions()
+
+        if self.thermal_transfer is not None:
+            temp = np.zeros(self.struct_nnodes, dtype=self.dtype)
+
+            for k in range(nfunctions):
+                psi_T = -self.aero_temps_ajp[:, k].copy()
+
+                self.thermal_transfer.applydTdtSTrans(psi_T, temp)
+                self.struct_temps_ajp[:, k] = temp
+
         return
 
     def add_aero_coordinate_derivative(self, scenario, step, dfdx):
@@ -1061,7 +1121,7 @@ class Body(Base):
             for k in range(nfunctions):
                 # Solve for psi_L - Note that dL/dfS is the identity matrix. Ensure that
                 # the vector is in contiguous memory.
-                psi_L = -self.struct_loads_adj_prod[:, k].copy()
+                psi_L = -self.struct_loads_ajp[:, k].copy()
                 self.transfer.applydLdxA0(psi_L, temp_xa)
                 self.aero_shape_term[:, k] += temp_xa
 
@@ -1070,7 +1130,7 @@ class Body(Base):
 
                 # Solve for psi_D - Note that dD/dua is the identity matrix
                 # Copy the values into contiguous memory.
-                psi_D = -self.aero_disps_adj_prod[:, k].copy()
+                psi_D = -self.aero_disps_ajp[:, k].copy()
                 self.transfer.applydDdxA0(psi_D, temp_xa)
                 self.aero_shape_term[:, k] += temp_xa
 

pyfuntofem/test_solver.py
@@ -67,9 +67,9 @@ class TestAerodynamicSolver(SolverInterface):
         The input to the adjoint computation is the right-hand-side for the force
         integration adjoint, internal to
 
-        aero_force_adj_prod = dL/dfA^{T} * psi_L
+        aero_force_ajp = dL/dfA^{T} * psi_L
 
-        aero_flux_adj_proc = dQ/dhA^{T} * psi_Q
+        aero_flux_ajp = dQ/dhA^{T} * psi_Q
 
 
 
@@ -203,14 +203,19 @@ class TestAerodynamicSolver(SolverInterface):
         """
 
         # Set the derivatives of the functions for the given scenario
-        # for findex, func in enumerate(scenario.functions):
-        #     for vindex, var in enumerate(self.aero_variables):
-        # for body in bodies:
-        #     psi_F = body.get_aero_loads_adjoint(scenario)
+        for findex, func in enumerate(scenario.functions):
+            for vindex, var in enumerate(self.aero_variables):
 
-        #     if psi_F is not None:
-        #         value = np.dot(psi_F[:, findex], self.c1[:, vindex])
-        #         func.add_derivative(var, value)
+                for body in bodies:
+                    aero_loads_ajp = body.get_aero_loads_ajp(scenario)
+                    if aero_loads_ajp is not None:
+                        value = np.dot(aero_loads_ajp[:, findex], self.c1[:, vindex])
+                        func.add_derivative(var, value)
+
+                    aero_flux_ajp = body.get_aero_flux_ajp(scenario)
+                    if aero_flux_ajp is not None:
+                        value = np.dot(aero_flux_ajp[:, findex], self.c2[:, vindex])
+                        func.add_derivative(var, value)
 
         return
 
@@ -276,21 +281,21 @@ class TestAerodynamicSolver(SolverInterface):
         """
 
         for body in bodies:
-            aero_loads_ap = body.get_aero_loads_adj_product(scenario)
-            aero_disps_ap = body.get_aero_disps_adj_product(scenario)
-            if aero_loads_ap is not None:
+            aero_loads_ajp = body.get_aero_loads_ajp(scenario)
+            aero_disps_ajp = body.get_aero_disps_ajp(scenario)
+            if aero_loads_ajp is not None:
                 for k, func in enumerate(scenario.functions):
-                    aero_disps_ap[:, k] = np.dot(self.Jac1.T, aero_loads_ap[:, k])
-                    # if func.analysis_type == "aerodynamic":
-                    #     adjD_rhs[:, k] += self.func_coefs1
+                    aero_disps_ajp[:, k] = np.dot(self.Jac1.T, aero_loads_ajp[:, k])
+                    if func.analysis_type == "aerodynamic":
+                        aero_disps_ajp[:, k] += self.func_coefs1
 
-            aero_flux_ap = body.get_aero_flux_adj_product(scenario)
-            aero_temps_ap = body.get_aero_temps_adj_product(scenario)
-            if aero_flux_ap is not None:
+            aero_flux_ajp = body.get_aero_flux_ajp(scenario)
+            aero_temps_ajp = body.get_aero_temps_ajp(scenario)
+            if aero_flux_ajp is not None:
                 for k, func in enumerate(scenario.functions):
-                    aero_temps_ap[:, k] = np.dot(self.Jac2.T, aero_flux_ap[:, k])
-                    # if func.analysis_type == "aerodynamic":
-                    #     adjD_rhs[:, k] += self.func_coefs1
+                    aero_temps_ajp[:, k] = np.dot(self.Jac2.T, aero_flux_ajp[:, k])
+                    if func.analysis_type == "aerodynamic":
+                        aero_temps_ajp[:, k] += self.func_coefs2
 
         fail = 0
         return fail
@@ -333,15 +338,15 @@ class TestAerodynamicSolver(SolverInterface):
         for body in bodies:
             body.initialize_adjoint_variables(scenario)
 
-            aero_loads_ap = body.get_aero_loads_adj_product(scenario)
-            if aero_loads_ap is not None:
-                shape = aero_loads_ap.shape
-                aero_loads_ap[:] = np.random.uniform(size=shape).astype(body.dtype)
+            aero_loads_ajp = body.get_aero_loads_ajp(scenario)
+            if aero_loads_ajp is not None:
+                shape = aero_loads_ajp.shape
+                aero_loads_ajp[:] = np.random.uniform(size=shape).astype(body.dtype)
 
-            aero_flux_ap = body.get_aero_flux_adj_product(scenario)
-            if aero_flux_ap is not None:
-                shape = aero_flux_ap.shape
-                aero_flux_ap[:] = np.random.uniform(size=shape).astype(body.dtype)
+            aero_flux_ajp = body.get_aero_flux_ajp(scenario)
+            if aero_flux_ajp is not None:
+                shape = aero_flux_ajp.shape
+                aero_flux_ajp[:] = np.random.uniform(size=shape).astype(body.dtype)
 
         # Compute one step of the adjoint
         self.initialize_adjoint(scenario, bodies)
@@ -368,13 +373,13 @@ class TestAerodynamicSolver(SolverInterface):
                 temp_pert_list.append(pert)
 
             # Take the dot-product with the exact adjoint computation
-            aero_disps_ap = body.get_aero_disps_adj_product(scenario)
-            if aero_disps_ap is not None:
-                adjoint_product += np.dot(aero_disps_ap[:, 0], disp_pert_list[-1])
+            aero_disps_ajp = body.get_aero_disps_ajp(scenario)
+            if aero_disps_ajp is not None:
+                adjoint_product += np.dot(aero_disps_ajp[:, 0], disp_pert_list[-1])
 
-            aero_temps_ap = body.get_aero_temps_adj_product(scenario)
-            if aero_temps_ap is not None:
-                adjoint_product += np.dot(aero_temps_ap[:, 0], temp_pert_list[-1])
+            aero_temps_ajp = body.get_aero_temps_ajp(scenario)
+            if aero_temps_ajp is not None:
+                adjoint_product += np.dot(aero_temps_ajp[:, 0], temp_pert_list[-1])
 
         # Sum up the result across all processors
         adjoint_product = self.comm.allreduce(adjoint_product)
@@ -388,18 +393,18 @@ class TestAerodynamicSolver(SolverInterface):
         fd_product = 0.0
         for body in bodies:
             aero_loads = body.get_aero_loads(scenario)
-            aero_loads_ap = body.get_aero_loads_adj_product(scenario)
-            if aero_loads is not None and aero_loads_ap is not None:
+            aero_loads_ajp = body.get_aero_loads_ajp(scenario)
+            if aero_loads is not None and aero_loads_ajp is not None:
                 aero_loads_copy = aero_loads_list.pop(0)
                 fd = (aero_loads - aero_loads_copy) / epsilon
-                fd_product += np.dot(fd, aero_loads_ap)
+                fd_product += np.dot(fd, aero_loads_ajp)
 
             aero_flux = body.get_aero_heat_flux(scenario)
-            aero_flux_ap = body.get_aero_flux_adj_product(scenario)
-            if aero_flux is not None and aero_flux_ap is not None:
+            aero_flux_ajp = body.get_aero_flux_ajp(scenario)
+            if aero_flux is not None and aero_flux_ajp is not None:
                 aero_flux_copy = aero_flux_list.pop(0)
                 fd = (aero_flux - aero_flux_copy) / epsilon
-                fd_product += np.dot(fd, aero_flux_ap)
+                fd_product += np.dot(fd, aero_flux_ajp)
 
         # Compute the finite-differenc approximation
         fd_product = self.comm.allreduce(fd_product)
@@ -541,14 +546,19 @@ class TestStructuralSolver(SolverInterface):
         """
 
         # Set the derivatives of the functions for the given scenario
-        # for findex, func in enumerate(scenario.functions):
-        #     for vindex, var in enumerate(self.struct_variables):
-        #         for body in bodies:
-        #             adjS_rhs = body.get_struct_adjoint_rhs(scenario)
+        for findex, func in enumerate(scenario.functions):
+            for vindex, var in enumerate(self.struct_variables):
 
-        #             if adjS_rhs is not None:
-        #                 value = -np.dot(adjS_rhs[:, findex], self.c1[:, vindex])
-        #                 func.add_derivative(var, value)
+                for body in bodies:
+                    struct_disps_ajp = body.get_struct_disps_ajp(scenario)
+                    if struct_disps_ajp is not None:
+                        value = np.dot(struct_disps_ajp[:, findex], self.c1[:, vindex])
+                        func.add_derivative(var, value)
+
+                    struct_temps_ajp = body.get_struct_temps_ajp(scenario)
+                    if struct_temps_ajp is not None:
+                        value = np.dot(struct_temps_ajp[:, findex], self.c2[:, vindex])
+                        func.add_derivative(var, value)
 
         return
 
@@ -612,21 +622,21 @@ class TestStructuralSolver(SolverInterface):
         """
 
         for body in bodies:
-            struct_disps_ap = body.get_struct_disps_adj_product(scenario)
-            struct_loads_ap = body.get_struct_loads_adj_product(scenario)
-            if struct_disps_ap is not None:
+            struct_disps_ajp = body.get_struct_disps_ajp(scenario)
+            struct_loads_ajp = body.get_struct_loads_ajp(scenario)
+            if struct_disps_ajp is not None:
                 for k, func in enumerate(scenario.functions):
-                    struct_loads_ap[:, k] = np.dot(self.Jac1.T, struct_disps_ap[:, k])
-                    # if func.analysis_type == "structural":
-                    #     adjD_rhs[:, k] += self.func_coefs1
+                    struct_loads_ajp[:, k] = np.dot(self.Jac1.T, struct_disps_ajp[:, k])
+                    if func.analysis_type == "structural":
+                        struct_loads_ajp[:, k] += self.func_coefs1
 
-            struct_temps_ap = body.get_struct_temps_adj_product(scenario)
-            struct_flux_ap = body.get_struct_flux_adj_product(scenario)
-            if struct_temps_ap is not None:
+            struct_temps_ajp = body.get_struct_temps_ajp(scenario)
+            struct_flux_ajp = body.get_struct_flux_ajp(scenario)
+            if struct_temps_ajp is not None:
                 for k, func in enumerate(scenario.functions):
-                    struct_flux_ap[:, k] = np.dot(self.Jac2.T, struct_temps_ap[:, k])
-                    # if func.analysis_type == "structural":
-                    #     adjD_rhs[:, k] += self.func_coefs1
+                    struct_flux_ajp[:, k] = np.dot(self.Jac2.T, struct_temps_ajp[:, k])
+                    if func.analysis_type == "structural":
+                        struct_flux_ajp[:, k] += self.func_coefs2
 
         fail = 0
         return fail
@@ -669,15 +679,15 @@ class TestStructuralSolver(SolverInterface):
         for body in bodies:
             body.initialize_adjoint_variables(scenario)
 
-            struct_disps_ap = body.get_struct_disps_adj_product(scenario)
-            if struct_disps_ap is not None:
-                shape = struct_disps_ap.shape
-                struct_disps_ap[:] = np.random.uniform(size=shape).astype(body.dtype)
+            struct_disps_ajp = body.get_struct_disps_ajp(scenario)
+            if struct_disps_ajp is not None:
+                shape = struct_disps_ajp.shape
+                struct_disps_ajp[:] = np.random.uniform(size=shape).astype(body.dtype)
 
-            struct_temps_ap = body.get_struct_temps_adj_product(scenario)
-            if struct_temps_ap is not None:
-                shape = struct_temps_ap.shape
-                struct_temps_ap[:] = np.random.uniform(size=shape).astype(body.dtype)
+            struct_temps_ajp = body.get_struct_temps_ajp(scenario)
+            if struct_temps_ajp is not None:
+                shape = struct_temps_ajp.shape
+                struct_temps_ajp[:] = np.random.uniform(size=shape).astype(body.dtype)
 
         # Compute one step of the adjoint
         self.initialize_adjoint(scenario, bodies)
@@ -704,13 +714,13 @@ class TestStructuralSolver(SolverInterface):
                 flux_pert_list.append(pert)
 
             # Take the dot-product with the exact adjoint computation
-            struct_loads_ap = body.get_struct_loads_adj_product(scenario)
-            if struct_loads_ap is not None:
-                adjoint_product += np.dot(struct_loads_ap[:, 0], load_pert_list[-1])
+            struct_loads_ajp = body.get_struct_loads_ajp(scenario)
+            if struct_loads_ajp is not None:
+                adjoint_product += np.dot(struct_loads_ajp[:, 0], load_pert_list[-1])
 
-            struct_flux_ap = body.get_struct_flux_adj_product(scenario)
-            if struct_flux_ap is not None:
-                adjoint_product += np.dot(struct_flux_ap[:, 0], flux_pert_list[-1])
+            struct_flux_ajp = body.get_struct_flux_ajp(scenario)
+            if struct_flux_ajp is not None:
+                adjoint_product += np.dot(struct_flux_ajp[:, 0], flux_pert_list[-1])
 
         # Sum up the result across all processors
         adjoint_product = self.comm.allreduce(adjoint_product)
@@ -724,18 +734,18 @@ class TestStructuralSolver(SolverInterface):
         fd_product = 0.0
         for body in bodies:
             struct_disps = body.get_struct_disps(scenario)
-            struct_disps_ap = body.get_struct_disps_adj_product(scenario)
-            if struct_disps is not None and struct_disps_ap is not None:
+            struct_disps_ajp = body.get_struct_disps_ajp(scenario)
+            if struct_disps is not None and struct_disps_ajp is not None:
                 struct_disps_copy = struct_disps_list.pop(0)
                 fd = (struct_disps - struct_disps_copy) / epsilon
-                fd_product += np.dot(fd, struct_disps_ap)
+                fd_product += np.dot(fd, struct_disps_ajp)
 
             struct_temps = body.get_struct_temps(scenario)
-            struct_temps_ap = body.get_struct_temps_adj_product(scenario)
-            if struct_temps is not None and struct_temps_ap is not None:
+            struct_temps_ajp = body.get_struct_temps_ajp(scenario)
+            if struct_temps is not None and struct_temps_ajp is not None:
                 struct_temps_copy = struct_temps_list.pop(0)
                 fd = (struct_temps - struct_temps_copy) / epsilon
-                fd_product += np.dot(fd, struct_temps_ap)
+                fd_product += np.dot(fd, struct_temps_ajp)
 
         # Compute the finite-differenc approximation
         fd_product = self.comm.allreduce(fd_product)