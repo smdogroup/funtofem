8e830ba72cdd95d76c61a5d78b03ff9b878d7ae5
sean-engelstad <sean.engelstad1@gmail.com>
2023-05-25 20:05:14 -0400

    funtofem morph and remesh aero shape tests

--- tests/fun3d_tests/test_funtofem_morph.py
+++ tests/fun3d_tests/test_funtofem_morph.py
@@ -16,15 +16,14 @@ fun3d_loader = importlib.util.find_spec("fun3d")
 has_fun3d = fun3d_loader is not None
 
 if has_fun3d:
-    from pyfuntofem.driver import FuntofemShapeDriver, Fun3dRemote
+    from pyfuntofem.driver import FuntofemShapeDriver
+    from pyfuntofem.interface import Fun3dInterface
 
 np.random.seed(1234567)
 
 comm = MPI.COMM_WORLD
 base_dir = os.path.dirname(os.path.abspath(__file__))
 csm_path = os.path.join(base_dir, "naca_wing.csm")
-
-analysis_file = os.path.join(base_dir, "run_fun3d_analysis.py")
 fun3d_dir = os.path.join(base_dir, "meshes")
 
 results_folder = os.path.join(base_dir, "results")
@@ -33,14 +32,14 @@ if comm.rank == 0:  # make the results folder if doesn't exist
         os.mkdir(results_folder)
 
 
-class TestFuntofemShape(unittest.TestCase):
+class TestFuntofemMorph(unittest.TestCase):
     """
     This class performs unit test on the oneway-coupled FUN3D driver
     which uses fixed struct disps or no struct disps
     TODO : in the case of an unsteady one, add methods for those too?
     """
 
-    FILENAME = "funtofem-shape-driver.txt"
+    FILENAME = "funtofem-morph-shape-driver.txt"
     FILEPATH = os.path.join(results_folder, FILENAME)
 
     def test_nominal(self):
@@ -48,12 +47,14 @@ class TestFuntofemShape(unittest.TestCase):
         # build the funtofem model with one body and scenario
         model = FUNtoFEMmodel("wing")
         # design the shape
-        fun3d_model = Fun3dModel.build(csm_file=csm_path, comm=comm)
+        fun3d_model = Fun3dModel.build_morph(csm_file=csm_path, comm=comm)
         aflr_aim = fun3d_model.aflr_aim
+        fun3d_aim = fun3d_model.fun3d_aim
 
-        aflr_aim.set_surface_mesh(ff_growth=1.2, min_scale=0.01, max_scale=5.0)
+        aflr_aim.set_surface_mesh(ff_growth=1.3, min_scale=0.01, max_scale=5.0)
         aflr_aim.set_boundary_layer(initial_spacing=0.001, thickness=0.01)
         Fun3dBC.inviscid(caps_group="wall", wall_spacing=0.001).register_to(fun3d_model)
+
         Fun3dBC.Farfield(caps_group="Farfield").register_to(fun3d_model)
         fun3d_model.setup()
         model.flow = fun3d_model
@@ -64,27 +65,33 @@ class TestFuntofemShape(unittest.TestCase):
         ).register_to(wing)
         wing.register_to(model)
         test_scenario = (
-            Scenario.steady("turbulent", steps=10)
+            Scenario.steady("turbulent", steps=5000)
             .set_temperature(T_ref=300.0, T_inf=300.0)
-            .fun3d_project("funtofem_CAPS")
+            .fun3d_project(fun3d_aim.project_name)
         )
+        test_scenario.adjoint_steps = 2000
         # test_scenario.get_variable("AOA").set_bounds(value=2.0)
+
         test_scenario.include(Function.lift()).include(Function.drag())
         test_scenario.register_to(model)
 
         # build the solvers and coupled driver
         solvers = SolverManager(comm)
-        fun3d_remote = Fun3dRemote(analysis_file, fun3d_dir, nprocs=48)
-        driver = FuntofemShapeDriver.remote(solvers, model, fun3d_remote)
+        solvers.flow = Fun3dInterface(
+            comm, model, fun3d_dir="meshes", auto_coords=False
+        )
+
+        # analysis driver for mesh morphing
+        driver = FuntofemShapeDriver.nominal(solvers, model)
 
         # run the complex step test on the model and driver
         max_rel_error = TestResult.finite_difference(
-            "fun3d+oneway-shape-euler-aeroelastic",
+            "funtofem-morph-euler-aeroelastic",
             model,
             driver,
-            TestFuntofemShape.FILEPATH,
-            both_adjoint=True,  # since the Fun3dOnewayDriver doesn't know function values until after solve_adjoint()
+            TestFuntofemMorph.FILEPATH,
             epsilon=1e-4,
+            both_adjoint=False,
         )
         self.assertTrue(max_rel_error < 1e-4)
 
@@ -93,5 +100,5 @@ class TestFuntofemShape(unittest.TestCase):
 
 if __name__ == "__main__":
     if comm.rank == 0:
-        open(TestFuntofemShape.FILEPATH, "w").close()
+        open(TestFuntofemMorph.FILEPATH, "w").close()
     unittest.main()
