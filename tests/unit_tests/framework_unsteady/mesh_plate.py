311ca9d54f238d40c6c141d1e3575f366baa4e51
seanfireball1 <sean.engelstad1@gmail.com>
2023-09-19 22:15:12 -0400

    new stiffened plate for tacs unsteady tests

--- tests/unit_tests/framework_unsteady/mesh_plate.py
+++ tests/unit_tests/framework_unsteady/mesh_plate.py
@@ -13,6 +13,7 @@ csm_path = os.path.join(base_dir, "input_files", "stiffened_plate.csm")
 if tacs_loader is not None and caps_loader is not None:
     from tacs import caps2tacs
 
+f2f_model = FUNtoFEMmodel("wing")
 tacs_model = caps2tacs.TacsModel.build(csm_file=csm_path, comm=comm)
 tacs_model.mesh_aim.set_mesh(  # need a refined-enough mesh for the derivative test to pass
     edge_pt_min=5,
@@ -23,21 +24,38 @@ tacs_model.mesh_aim.set_mesh(  # need a refined-enough mesh for the derivative t
 ).register_to(
     tacs_model
 )
+f2f_model.structural = tacs_model
 # setup the material and shell properties
 aluminum = caps2tacs.Isotropic.aluminum().register_to(tacs_model)
 
 nx = int(tacs_model.get_config_parameter("nx"))
 ny = int(tacs_model.get_config_parameter("ny"))
 
+wing = Body.aeroelastic("wing")
+
+prop = caps2tacs.ShellProperty(
+    caps_group=f"thick", material=aluminum, membrane_thickness=0.05
+).register_to(tacs_model)
+Variable.from_caps(prop).set_bounds(lower=0.01, upper=0.1).register_to(wing)
+
 for ix in range(1, nx + 1):
-    for iy in range(1, ny + 1):
-        caps2tacs.ShellProperty(
-            caps_group=f"panel{ix}-{iy}", material=aluminum, membrane_thickness=0.05
-        ).register_to(tacs_model)
+    prop = caps2tacs.ShellProperty(
+        caps_group=f"stiffx{ix}", material=aluminum, membrane_thickness=0.05
+    ).register_to(tacs_model)
+    Variable.from_caps(prop).set_bounds(lower=0.01, upper=0.1).register_to(wing)
+
+for iy in range(1, ny + 1):
+    prop = caps2tacs.ShellProperty(
+        caps_group=f"stiffy{iy}", material=aluminum, membrane_thickness=0.05
+    ).register_to(tacs_model)
+    Variable.from_caps(prop).set_bounds(lower=0.01, upper=0.1).register_to(wing)
 
 # add remaining information to tacs model
 caps2tacs.PinConstraint("fix").register_to(tacs_model)
-caps2tacs.TemperatureConstraint("temp", temperature=0).register_to(tacs_model)
+caps2tacs.TemperatureConstraint("fix", temperature=0).register_to(tacs_model)
+
+# register the wing body to the model
+wing.register_to(f2f_model)
 
 # setup the tacs model
 tacs_model.setup()
