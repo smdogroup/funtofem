6c8fdc82fa601a99beba903117e962b5103ea4c5
seanfireball1 <sean.engelstad1@gmail.com>
2024-03-24 21:39:07 -0400

    save funtofem states

--- funtofem/driver/_funtofem_driver.py
+++ funtofem/driver/_funtofem_driver.py
@@ -47,6 +47,7 @@ class FUNtoFEMDriver(object):
         transfer_settings=None,
         model=None,
         debug=False,
+        reload_funtofem_states=False,
     ):
         """
         Parameters
@@ -59,6 +60,8 @@ class FUNtoFEMDriver(object):
             options of the load and displacement transfer scheme
         model: :class:`~funtofem_model.FUNtoFEMmodel`
             The model containing the design data
+        reload_funtofem_states: bool
+            whether to save and reload funtofem states
         """
 
         # add the comm manger
@@ -72,6 +75,7 @@ class FUNtoFEMDriver(object):
         if transfer_settings is None:
             transfer_settings = TransferSettings()
         self.transfer_settings = transfer_settings
+        self.reload_funtofem_states = reload_funtofem_states
 
         # communicator
         self.comm = comm_manager.master_comm
@@ -252,6 +256,10 @@ class FUNtoFEMDriver(object):
             fail = solver.initialize(scenario, bodies)
             if fail != 0:
                 return fail
+            
+        # reload funtofem states (want this to be after TACS/struct solvers set size of states in)
+        if self.reload_funtofem_states:
+            self.model.load_funtofem_states(self.comm)
         return 0
 
     def _initialize_adjoint(self, scenario, bodies):
@@ -270,6 +278,12 @@ class FUNtoFEMDriver(object):
     def _post_forward(self, scenario, bodies):
         for solver in self.solvers.solver_list:
             solver.post(scenario, bodies)
+
+        # save the funtofem states
+        if self.reload_funtofem_states:
+            self.model.save_funtofem_states(self.comm)
+
+        return
 
     def _post_adjoint(self, scenario, bodies):
         for solver in self.solvers.solver_list:
---
+++ tests/unit_tests/basic_functionality/test_save_funtofem_states.py
@@ -0,0 +1,69 @@
+import numpy as np
+from mpi4py import MPI
+from funtofem import TransferScheme
+
+from funtofem.model import FUNtoFEMmodel, Variable, Scenario, Body, Function
+from funtofem.interface import (
+    TestAerodynamicSolver,
+    TestStructuralSolver,
+    SolverManager,
+)
+from funtofem.driver import FUNtoFEMnlbgs, TransferSettings
+
+import unittest
+
+comm = MPI.COMM_WORLD
+
+
+class TestReloadFuntofemStates(unittest.TestCase):
+    def test_reload_funtofem_states(self):
+        # build the funtofem model with an unsteady scenario
+        model = FUNtoFEMmodel("test")
+        plate = Body.aerothermoelastic("plate")
+        plate.register_to(model)
+        test_scenario = Scenario.steady("test", steps=10)
+        Function.test_struct().register_to(test_scenario)
+        Function.test_aero().register_to(test_scenario)
+        test_scenario.register_to(model)
+
+        # build a funtofem driver
+        solvers = SolverManager(comm)
+        solvers.flow = TestAerodynamicSolver(comm, model)
+        solvers.structural = TestStructuralSolver(comm, model)
+        driver = FUNtoFEMnlbgs(
+            solvers,
+            transfer_settings=TransferSettings(npts=10),
+            model=model,
+            reload_funtofem_states=True,
+        )
+
+        # run coupled analysis
+        driver.solve_forward()
+
+        # print out the coupled states
+        struct_disps_f = plate.struct_disps[test_scenario.id] * 1.0
+        print(f"final struct disps = {struct_disps_f[:5]}")
+        struct_temps_f = plate.struct_temps[test_scenario.id] * 1.0
+        print(f"final struct temps = {struct_temps_f[:5]}")
+
+        # clear the states
+        plate.struct_disps[test_scenario.id] *= 0.0
+        plate.struct_temps[test_scenario.id] *= 0.0
+        print(f"cleared struct disps = {plate.struct_disps[test_scenario.id][:5]}")
+        print(f"cleared struct temps = {plate.struct_temps[test_scenario.id][:5]}")
+
+        # reload the states
+        model.load_funtofem_states(comm)
+        loaded_struct_disps = plate.struct_disps[test_scenario.id] * 1.0
+        print(f"loaded struct disps = {loaded_struct_disps[:5]}")
+        loaded_struct_temps = plate.struct_temps[test_scenario.id] * 1.0
+        print(f"loaded struct temps = {loaded_struct_temps[:5]}")
+
+        # compute the error in the final and reloaded states
+        assert np.max(struct_disps_f) == np.max(loaded_struct_disps)
+        assert np.max(struct_temps_f) == np.max(loaded_struct_temps)
+        return
+
+
+if __name__ == "__main__":
+    unittest.main()
