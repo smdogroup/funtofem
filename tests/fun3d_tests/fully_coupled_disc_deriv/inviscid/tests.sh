a07444c4345996921e552c432e300f31a778283b
sean-engelstad <sean.engelstad1@gmail.com>
2024-05-02 13:20:04 -0400

    update the ssw and flat plate tests

--- tests/fun3d_tests/fully_coupled_disc_deriv/inviscid/tests.sh
+++ tests/fun3d_tests/fully_coupled_disc_deriv/inviscid/tests.sh
@@ -1,4 +1,5 @@
 python test_fun3d_grid_deformation.py 2>&1 > grid_deformation.txt
+python test_aero_loads_quads_tc.py 2>&1 > aero_loads_tc.txt
 python test_aero_loads_quads.py 2>&1 > aero_loads.txt
 python test_flow_states_quads.py 2>&1 > flow_states.txt
 python test_fun3d_tacs_FD.py > 1-derivs.txt
