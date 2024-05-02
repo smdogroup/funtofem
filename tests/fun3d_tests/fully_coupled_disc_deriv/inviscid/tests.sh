d06af3e3a7055ff03d3af12dc5191a7dac82bd8c
sean-engelstad <sean.engelstad1@gmail.com>
2024-05-02 10:18:24 -0400

    add tests.sh

---
+++ tests/fun3d_tests/fully_coupled_disc_deriv/inviscid/tests.sh
@@ -0,0 +1,4 @@
+python test_fun3d_grid_deformation.py 2>&1 > grid_deformation.txt
+python test_aero_loads_quads.py 2>&1 > aero_loads.txt
+python test_flow_states_quads.py 2>&1 > flow_states.txt
+python test_fun3d_tacs_FD.py > 1-derivs.txt
