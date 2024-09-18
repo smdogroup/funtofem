8b53ecba6c26a1087811d44f24f8c3def6a567a2
Burke, Brian <bburke38@gatech.edu>
2024-09-18 12:32:21 -0400

    Update _funtofem_driver.py

--- funtofem/driver/_funtofem_driver.py
+++ funtofem/driver/_funtofem_driver.py
@@ -20,9 +20,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__all__ = ["FUNtoFEMDriver"]
-
 from __future__ import annotations
+
+__all__ = ["FUNtoFEMDriver"]
 
 import numpy as np
 from mpi4py import MPI
