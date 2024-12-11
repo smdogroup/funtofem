a9243f4bd4a3112f60308b75fdbe5ceed6b33e03
Tim Brooks <41971846+timryanb@users.noreply.github.com>
2024-12-11 17:07:12 -0500

    changing mphys req version to 2.0

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ for e in exts:
 optional_dependencies = {
     "testing": ["testflo>=1.4.7"],
     "docs": ["sphinx"],
-    "mphys": ["mphys>=2.0.0b1", "openmdao>=3.25.0"],
+    "mphys": ["mphys>=2.0.0", "openmdao>=3.25.0"],
     "plots": ["niceplots"],
 }
 
